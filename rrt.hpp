76ece5e03172c1df3798355fa64105ef88ca8020
kobikelemen <kobi.kelemen20@gmail.com>
2021-10-14 14:44:43 +0100

    added get_random + other fixes

--- rrt.hpp
+++ rrt.hpp
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 
 class Point
 {
@@ -48,24 +48,24 @@ class Point
 
 class Node
 {
-    vector<Node> connections;
 public:
-    Position pos;
+    std::vector<Node> connections;
+    Point pos;
     int depth;
-    Node(vector<Node> connections_, Position pos_, int _depth){
+    Node(std::vector<Node> connections_, Point pos_, int _depth){
         connections = connections_;
         pos = pos_;
         depth = _depth;
     }
     void add_connections(Node n){
-        connections.pushback(n);
+        connections.push_back(n);
     }
 
 };
 
 class Graph
 {
-    vector<Node> node_list;
+    std::vector<Node> node_list;
 public:
     Node *nearest_node(Point p)
     {
@@ -93,13 +93,66 @@ public:
             
         }
     }
+    int size(){
+        return node_list.size();
+    }
 
 };
 
 
-Point get_random(vector<Point> boundary, vector<Point> obstacles)
+float max(std::vector<float> x){
+    float max = 0;
+    for (int i=0; i < x.size(); i++){
+        if (x[i] > max){
+            max = x[i];
+        }
+    }
+    return max;
+}
+
+float min(std::vector<float> x){
+    float min = 1e5;
+    for (int i=0; i < x.size(); i++){
+        if (x[i] < min){
+            min = x[i];
+        }
+    }
+    return min;
+}
+
+
+Point get_random(std::vector<Point> boundary, std::vector<Point> obstacles)
 {
 // get random point within boudnary thats not in an obstacle
+    std::vector<float> x_boundaries;
+    std::vector<float> y_boundaries;
+    std::vector<float> z_boundaries;
+    for (int i=0; i < boundary.size(); i++){
+        x_boundaries.push_back(boundary[i].x);
+        y_boundaries.push_back(boundary[i].y);
+        z_boundaries.push_back(boundary[i].z);
+    }
+    bool check = false;
+    float x_check, y_check, z_check;
+    Point p_max(max(x_boundaries), max(y_boundaries), max(z_boundaries));
+    Point p_min(min(x_boundaries), max(y_boundaries), max(z_boundaries));
+
+    while (!check){
+        x_check = rand() % (p_max.x - p_min.x);
+        x_check += p_min.x;
+        y_check = rand() % (p_max.y - p_min.y);
+        y_check += p_min.y;
+        z_check = rand() % (p_max.z - p_min.z);
+        z_check += p_min.z;
+
+        if (x_check < p_max.x && x_check > p_min.x && y_check < p_max.y && y_check > p_min.y && z_check > p_max.z && z_check < p_min.z){
+            // havent checked if in obstacle yet ... !
+            Point p(x_check, y_check, z_check);
+            return p;
+        }
+
+
+    }
 }
 
 Point chain(Point nearest, Point new_p, float max_step_size)
@@ -111,19 +164,37 @@ Point chain(Point nearest, Point new_p, float max_step_size)
         Point unit_vec = diff_vec.unit_vec();
         diff_vec = unit_vec.scalar_mult(magn);
     }
-    Point new_point = nearest_node.add_vec(diff_vec);
+    Point new_point = nearest.add_vec(diff_vec);
     return new_point;
 }
 
-vector<Node> get_path(Graph G, Node end_node)
-{
-
+std::vector<Node> get_path(Graph G, Node end_node)
+{   
+    int depth = end_node.depth;
+    Node node = end_node;
+    std::vector<Node> reverse_path;
+    while (depth != 1){
+        for (int i=0; i<node.connections.size(), i++){
+            Node check_node = node.connections[i];
+            if (check_node.depth == node.depth - 1) {
+                node = check_node;
+                depth = node.depth;
+                reverse_path.push_back(node);
+            }
+        }
+    }
+    std::reverse(reverse_path.begin(), reverse_path.end());
+    return reverse_path; // not reverse anymore ...
 }
 
 
 
 
-Graph rrt(Node goal, int lim, Graph G, float step_size, Node start_node)
+std::vector<Node> rrt(
+    Node goal, Node start_node, int lim, Graph G, 
+    float step_size(std::vector<Point> boundary, 
+    std::vector<Point> obstacles
+    )
 {
     int counter = 0;
     start_node.depth = 1;
@@ -133,14 +204,16 @@ Graph rrt(Node goal, int lim, Graph G, float step_size, Node start_node)
         Node *nearest;
         nearest = G.nearest_node(new_p);
         Point new_point = chain(*nearest.pos, new_p, step_size);
-        vector<Node> new_point_connections = {*nearest};
+        std::vector<Node> new_point_connections = {*nearest};
         Node new_node(new_point_connections, new_point);
         *nearest.add_connection(new_node);
         new_node.depth = *nearest.depth + 1;
         G.add_node(new_node);
         if (new_point.in_goal()){
-            return G;
+            std::vector<Node> path = get_path(G, new_node);
+            return path;
         }
         counter++;
     }
+
 }
