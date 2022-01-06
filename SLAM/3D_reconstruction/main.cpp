e9e67903c9b116a857d23888e5ddcab4d14fd37e
kobikelemen <kobi.kelemen20@gmail.com>
2022-01-06 20:09:41 +0000

    Fully translated + started reconstruction using triangulation

--- SLAM/3D_reconstruction/main.cpp
+++ SLAM/3D_reconstruction/main.cpp
@@ -1,5 +1,4 @@
 #include "scene_graph.hpp"
-// #include <map>
 #include <opencv2/opencv.hpp>
 #include <opencv2/sfm.hpp>
 #include <iostream>
@@ -11,20 +10,77 @@
 // (pkg-config --cflags --libs opencv has links)
 
 
-void save_frames()
+void save_frames(Scene_graph &g)
 {
     cv::VideoCapture house_tour("/Users/kobikelemen/Documents/programming/c++/open_test/house_tour.mp4");
     cv::namedWindow("img", cv::WINDOW_AUTOSIZE);
-    for (int frame_num=0; frame_num < house_tour.get(cv::CAP_PROP_FRAME_COUNT); frame_num++) {
-        cv::Mat f;
-        house_tour >> f;
-        cv::imshow("img", f);
-        if (cv::waitKey(30) == 27) { // break using ESC key
-            break;
+    cv::Ptr<cv::ORB> detector = cv::ORB::create(2000);
+    std::vector<Image> first_two;
+
+    for (int frame_num=0; frame_num <= 30; frame_num++) {
+        if (frame_num % 30 == 0) {
+            cv::Mat f;
+            house_tour >> f;
+            std::vector<cv::KeyPoint> kp;
+            cv::Mat desc;
+            detector->detectAndCompute(f, cv::noArray(), kp, desc);
+            int t = frame_num/30;
+            Image img(t, kp, desc, f);
+            first_two.push_back(img);
+            g.add_frame(img);
+        }
+    }
+    g.initialise(first_two[0], first_two[1]);
+
+    for (int frame_num=31; frame_num < house_tour.get(cv::CAP_PROP_FRAME_COUNT); frame_num++) {
+        if (frame_num % 30 == 0) {
+            cv::Mat f;
+            house_tour >> f;
+            std::vector<cv::KeyPoint> kp;
+            cv::Mat desc;
+            detector->detectAndCompute(f, cv::noArray(), kp, desc);
+            int t = frame_num/30;
+            Image img(t, kp, desc, f);
+            g.add_frame(img);
+
+            cv::Mat imgkp;
+            cv::drawKeypoints(f, kp, imgkp);
+            cv::imshow("img", imgkp);
+
+            if (cv::waitKey(30) == 27) { // break using ESC key
+                break;
+            }
         }
     }
     cv::destroyAllWindows();
 }
+
+bool check(cv::DMatch i, cv::DMatch j) {
+    return i.distance < j.distance;
+}
+
+void image_correspondances(Scene_graph &g)
+{
+    std::vector<Image> imgs = g.get_frames();
+    cv::BFMatcher matcher(cv::NORM_HAMMING2, true);
+
+    for (Image img : imgs) {
+        for (Image check_img : imgs) {
+            if (img.frame_no != check_img.frame_no) {
+                std::vector<cv::DMatch> matches;
+                matcher.match(img.descriptor, check_img.descriptor, matches);
+                std::sort(matches.begin(),matches.end(), check);
+                if (matches.front().distance < 20) {
+                    g.add_connection(img.frame_no, check_img.frame_no);
+                }
+                // MISSING GEOMETRIC VERIFICATINO USING RANSAC
+                // MAYBE DIFFERENT MATCHER HANDLES THAT?
+            }
+        }
+    }
+}
+
+
 
 
 
@@ -37,7 +93,12 @@ int main()
     // std::vector<cv::Point2f> p1, p2;
     // cv::Mat inliers;
     // cv::Mat essential = cv::findEssentialMat(p1, p2);
-    save_frames();
+    Scene_graph g;
+    save_frames(g);
     return 0;
 
 }
+
+
+
+
--- SLAM/3D_reconstruction/scene_graph.hpp
+++ SLAM/3D_reconstruction/scene_graph.hpp
@@ -1,15 +1,20 @@
 #include <map>
-// #include <opencv2/opencv.hpp>
+// #include </Users/kobikelemen/Documents/programming/c++/opencv2/include/opencv2/opencv.hpp>
+// #include "/usr/local/opt/opencv@2/include/opencv2/opencv.hpp"
+
 #include <vector>
 
 
 
 class Image
 {
+public:
     int frame_no;
+    cv::Mat frame;
     std::vector<int> connections;
-    // cv::keypoint
-    Image();
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptor;
+    Image(int frame_no,  std::vector<cv::KeyPoint> keypoints, cv::Mat descriptor, cv::Mat frame);
     void add_connection(int c);
     std::vector<int> get_connection_indexes();
 
@@ -18,5 +23,12 @@ class Image
 
 class Scene_graph
 {
+    std::vector<Image> frames;
+public:
+    void add_frame(Image f);
+    std::vector<Image> get_frames(void);
+    Image* get_frame(int frame_num);
     Scene_graph();
+    void add_connection(int f1, int f2);
+    void initialise(Image img1, Image img2); // fundamental matrix + triangulation
 };
